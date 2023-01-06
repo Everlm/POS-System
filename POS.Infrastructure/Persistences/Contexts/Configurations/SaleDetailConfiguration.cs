64223228bc1a7bd7ab42667dda37e14ee5669ff2
Ever LM <everlm17@gmail.com>
2023-01-06 15:07:38 -0500

    Add Contexts Configuration and Injection Extensions

---
+++ POS.Infrastructure/Persistences/Contexts/Configurations/SaleDetailConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configurations
+{
+    public class SaleDetailConfiguration : IEntityTypeConfiguration<SaleDetail>
+    {
+        public void Configure(EntityTypeBuilder<SaleDetail> builder)
+        {
+            builder.Property(e => e.Discount).HasColumnType("decimal(18, 2)");
+
+            builder.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+            builder.HasOne(d => d.Product)
+                .WithMany(p => p.SaleDetails)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__SaleDetai__Produ__571DF1D5");
+
+            builder.HasOne(d => d.Sale)
+                .WithMany(p => p.SaleDetails)
+                .HasForeignKey(d => d.SaleId)
+                .HasConstraintName("FK__SaleDetai__SaleI__5812160E");
+        }
+    }
+}
---
+++ POS.Infrastructure/Persistences/Contexts/Configurations/UsersBranchOfficeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+
+namespace POS.Infrastructure.Persistences.Contexts.Configurations
+{
+    public class UsersBranchOfficeConfiguration : IEntityTypeConfiguration<UsersBranchOffice>
+    {
+        public void Configure(EntityTypeBuilder<UsersBranchOffice> builder)
+        {
+            builder.HasKey(e => e.UserBranchOfficeId)
+                    .HasName("PK__UsersBra__7D1E804A3AEDE667");
+
+            builder.HasOne(d => d.BranchOffice)
+                .WithMany(p => p.UsersBranchOffices)
+                .HasForeignKey(d => d.BranchOfficeId)
+                .HasConstraintName("FK__UsersBran__Branc__5DCAEF64");
+
+            builder.HasOne(d => d.User)
+                .WithMany(p => p.UsersBranchOffices)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__UsersBran__UserI__5EBF139D");
+        }
+    }
+}
