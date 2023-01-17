2e4806ba38d236a2fff107e9903232811400aaca
Ever LM <everlm17@gmail.com>
2023-01-17 09:10:44 -0500

    Add new features

---
+++ POS.Infrastructure/Commons/Bases/BasePaginationRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace POS.Infrastructure.Commons.Bases
+{
+    public class BasePaginationRequest
+    {
+        public int NumPage { get; set; } = 1;
+        public int NumRecordsPage { get; set; } = 10;
+        public string Order { get; set; } = "asc";
+        public string? Sort { get; set; } = null;
+        private readonly int NumMaxRecordsPage = 50;
+        public int Records
+        {
+            get => NumRecordsPage;
+            set
+            {
+                NumRecordsPage = (value > NumMaxRecordsPage) ? NumRecordsPage : value;
+            }
+        }
+
+
+    }
+}
---
+++ POS.Infrastructure/Persistences/Repositories/GenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using POS.Infrastructure.Commons.Bases;
+using POS.Infrastructure.Helpers;
+using POS.Infrastructure.Persistences.Interfaces;
+using System.Linq.Dynamic.Core;
+
+namespace POS.Infrastructure.Persistences.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected IQueryable<TDTO> Ordering<TDTO>(BasePaginationRequest request, IQueryable<TDTO> queryable, bool pagination = false) where TDTO : class
+        {
+            IQueryable<TDTO> queryDto = request.Order == "desc" ? queryable.OrderBy($"{request.Sort} descending") : queryable.OrderBy($"{request.Sort} ascending");
+
+            if (pagination) queryDto = queryDto.Paginate(request);
+
+            return queryDto;
+
+        }
+    }
+}
