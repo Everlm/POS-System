24bc9f50c3b9f1049c5967421e89811b7caa0e9a
Ever LM <everlm17@gmail.com>
2023-02-01 11:20:42 -0500

    Add Category UnitTest

--- POS.Infrastructure/Persistences/Repositories/UnitOfWork.cs
+++ POS.Infrastructure/Persistences/Repositories/UnitOfWork.cs
@@ -6,17 +6,22 @@ namespace POS.Infrastructure.Persistences.Repositories
     public class UnitOfWork : IUnitOfWork
     {
         private readonly POSContext _context;
-        
+        public UnitOfWork(POSContext context)
+        {
+            _context = context;
+            Category = new CategoryRepository(_context);
+
+        }
         public ICategoryRepository Category { get; private set; }
 
         public void SaveChanges()
         {
-           _context.SaveChanges();
+            _context.SaveChanges();
         }
 
         public async Task SaveChangesAsync()
         {
-             await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
 
         public void Dispose()
@@ -24,11 +29,6 @@ namespace POS.Infrastructure.Persistences.Repositories
             _context.Dispose();
         }
 
-        public UnitOfWork(POSContext context)
-        {
-            _context = context;
-            Category = new CategoryRepository(_context);
 
-        }
     }
 }
---
+++ POS.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace POS.Test
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(ConfigurationBuilder =>
+            {
+                var IntegrationConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+                ConfigurationBuilder.AddConfiguration(IntegrationConfiguration);
+
+            });
+        }
+    }
+}
